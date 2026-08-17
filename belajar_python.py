bf3a00c435b64bf269ad5bf1a269c20e008181c2
khusni-tilaka <khusni.ja'far@tilaka.id>
2026-08-17 18:40:36 +0700

    add material

--- belajar_python.py
+++ belajar_python.py
@@ -1,3 +1,12 @@
 print("Halo, Dunia!")
 # total = (jumlah buku x harga buku) + (jumlah pulpen x harga pulpen)
 print("total =", (1*5) + (1*2))
+angka_teks = "456"
+print(int(angka_teks) + 50)
+print(float(angka_teks))
+print(bool(1))
+print(list("Python"))
+buah = ["apel", "pisang", "mangga"]
+print(buah[0])  # apel
+print(buah[2])  # mangga
+print(buah[-3]) # mangga (indeks negatif dari belakang)

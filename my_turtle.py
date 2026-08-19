856a258fb145f1839d3e7dafcacb10675e01704b
khusni-tilaka <khusni.ja'far@tilaka.id>
2026-08-19 18:47:42 +0700

    add material

---
+++ my_turtle.py
@@ -0,0 +1,20 @@
+import turtle
+
+t = turtle.Pen()
+t.shape("turtle")
+t.pencolor("blue")
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+
+turtle.exitonclick()  # Jendela akan tetap terbuka sampai kamu klik
